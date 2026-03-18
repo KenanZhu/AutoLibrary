2d0782c368c2a0692559947bf32574d49c226589
KenanZhu <3471685733@qq.com>
2026-03-18 10:17:09 +0800

    refactor(AppInitializer): 将初始化逻辑提取到 AppInitializer 模块中
    - 本次提交将 Main.py 中的 ConfigManager, LogManager 等初始化逻辑提取到 AppInitializer 模块中
    - 更改默认的配置文件路径从 config 目录变为 configs 目录，并考虑兼容性问题

--- src/Main.py
+++ src/Main.py
@@ -10,31 +10,14 @@ See the LICENSE file for details.
 import os
 import sys
 
-from PySide6.QtCore import QTranslator, QStandardPaths, QDir
+from PySide6.QtCore import QTranslator
 from PySide6.QtWidgets import QApplication
 
 from gui.ALMainWindow import ALMainWindow
 from gui.resources import ALResource
 
-from utils.ConfigManager import instance as configInstance
-from utils.LogManager import instance as logInstance
+from utils.AppInitializer import initializeApp
 
-
-def initializeConfigManager():
-
-    app_dir = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.AppDataLocation)
-    config_dir = os.path.join(app_dir, "config")
-    if not QDir(config_dir).exists():
-        QDir().mkpath(config_dir)
-    configInstance(config_dir)
-
-def initializeLogManager():
-
-    app_dir = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.AppDataLocation)
-    log_dir = os.path.join(app_dir, "logs")
-    if not QDir(log_dir).exists():
-        QDir().mkpath(log_dir)
-    logInstance(log_dir)
 
 def main():
 
@@ -44,12 +27,11 @@ def main():
         app.installTranslator(translator)
     app.setStyle('Fusion')
     app.setApplicationName("AutoLibrary")
-    initializeConfigManager()
-    initializeLogManager()
+    if not initializeApp():
+        sys.exit(-1)
     window = ALMainWindow()
     window.show()
     sys.exit(app.exec_())
-
 
 if __name__ == "__main__":
 
